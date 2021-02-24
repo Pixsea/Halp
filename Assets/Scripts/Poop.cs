adcfcb65010b31f27c95ed2f5658e4c26e1447e2
Pixsea <jiin@uci.edu>
2021-02-23 20:23:29 -0800

    Edited scene

---
+++ Assets/Scripts/Poop.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Poop : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int p = 0;
+        p.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
